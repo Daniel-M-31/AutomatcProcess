16a8d6cae728fefa733ac9cf24adb4774f35d3d2
Daniel-M-31 <danielfeijao17@gmail.com>
2026-04-30 17:05:22 -0300

    feat: Add superuser authentication for scheduling and new task listing

--- cron_shutdown.py
+++ cron_shutdown.py
@@ -1,16 +1,56 @@
 import subprocess as sp
+import getpass as gp
+
 
 def cron(h, m):
     cmand = f"{m} {h} * * * /sbin/shutdown -h now\n"
+    pswd = gp.getpass('Enter sudo password: ')
 
     try:
         #Search programmed tasks
-        sch = sp.run(['contrab', '-l'], capture_output=True, text=True)
-        pgd_tks = sch.stdout if sch.returncode else ''
+        sch = sp.run(['sudo', '-S', 'crontab', '-l'], input=(pswd+'\n'), capture_output=True, text=True)
+        del pswd
+        pgd_tks = sch.stdout if sch.returncode ==0 else ''
 
+        if cmand in pgd_tks:
+            print('[!] Task already exists')
+            return
+        if pgd_tks and not pgd_tks.endswith('\n'):
+            pgd_tks += '\n'
 
-        prss = sp.Popen(['crontab', '-'], stdin=sp.PIPE)
-        prss.communicate(input=cmand.encode())
+        upt_tsks = pgd_tks + cmand
+
+        #Task executor
+        prss = sp.Popen(['sudo', 'crontab', '-'], stdin=sp.PIPE)
+        prss.communicate(input=upt_tsks.encode())
+
+        print(f'[OK] Shutdown scheduled: {h}:{m}]\n\n')
 
     except:
-        print(f"{Exception()}")
+        print(f"[!] Unexpected error occurred")
+
+def lst():
+    tsk = ['', '']
+    pswd = gp.getpass('Enter sudo password: ')
+    try:
+        sch = sp.run(['sudo', '-S', 'crontab', '-l'], input=(pswd + '\n'), capture_output=True, text=True)
+        del pswd
+        pgd_tks = sch.stdout if sch.returncode == 0 else ''
+
+        if pgd_tks and not pgd_tks.endswith('\n'):
+            pgd_tks += '\n'
+
+        for i in sch.stdout:
+            if i.isnumeric():
+                if len(tsk[1])<2:
+                    tsk[1] += i
+                else: tsk[0] += i
+
+                if len(tsk[1])==2 and len(tsk[0])==2:
+                    print(f'#Shutdown set for {tsk[0]}:{tsk[1]}\n')
+
+            if i == '\n':
+                tsk = ['', '']
+
+    except:
+        print(f"[!] Unexpected error occurred")
